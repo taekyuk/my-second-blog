96dbc1dafd08550788348725c759db10054307a8
taekyu <taekyu@taekyu.com>
2016-08-18 03:48:10 +0900

    Tried to fixing bugs and added comment

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    
 
+urlpatterns=[
+    
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 ]
