a212740d4bfcdb82ca4d396cdd19707cb58e5d53
taekyu <taekyu@taekyu.com>
2016-08-19 03:28:17 +0900

    created login page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.utils import timezone
-#from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
@@ -8,15 +7,15 @@ from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from .models import Post, Comment
 
-#@login_required
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts': posts})
-#@login_required
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html',{'post': post})
-#@login_required
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,7 +28,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form':form})
-#@login_required
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == "POST":
@@ -43,7 +42,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form':form})
-#@login_required
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == "POST":
@@ -57,19 +56,19 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form':form})
 
-#@login_required
+@login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('blog.views.post_detail', pk=comment.post.pk)
 
-#@login_required
+@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
     comment.delete()
     return redirect('blog.views.post_detail', pk=post_pk)
-#@login_required
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
